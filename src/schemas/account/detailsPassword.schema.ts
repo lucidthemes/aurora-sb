9a5e5332698099c98588b78a09861892b28388e4
Lucid Themes <lucidtemplates@gmail.com>
2026-02-16 15:49:22 +0000

    feat(schemas): add account details password form schema

---
+++ src/schemas/account/detailsPassword.schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const DetailsPasswordFormSchema = z
+  .object({
+    password: z.string().min(8, 'Password needs to be longer than 8 characters'),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
+
+export type DetailsPasswordForm = z.infer<typeof DetailsPasswordFormSchema>;
