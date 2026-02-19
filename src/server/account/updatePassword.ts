47ec99758e2b4fa7e14029dc96c4950c66f9aed1
Lucid Themes <lucidtemplates@gmail.com>
2026-02-19 17:50:20 +0000

    refactor(server): remove account update password return email

--- src/server/account/updatePassword.ts
+++ src/server/account/updatePassword.ts
@@ -1,26 +1,13 @@
 import { supabase } from '@lib/supabase/client';
-import { DetailsPasswordFormReturnSchema } from '@schemas/account/detailsPassword.schema';
-import type { DetailsPasswordForm, DetailsPasswordFormReturn } from '@schemas/account/detailsPassword.schema';
+import type { DetailsPasswordForm } from '@schemas/account/detailsPassword.schema';
 import { FetchError } from '@services/errors/fetchError';
 
-export async function updateAccountDetailsPassword(formData: DetailsPasswordForm): Promise<DetailsPasswordFormReturn> {
-  const { data, error } = await supabase.auth.updateUser({
+export async function updateAccountDetailsPassword(formData: DetailsPasswordForm) {
+  const { error } = await supabase.auth.updateUser({
     password: formData.password,
   });
 
   if (error) {
     throw new FetchError('UPDATE_PASSWORD_FAILED', error.message);
   }
-
-  if (!data.user) {
-    throw new FetchError('UPDATE_PASSWORD_NO_USER', 'No user found');
-  }
-
-  const parsed = DetailsPasswordFormReturnSchema.safeParse(data.user.email);
-
-  if (!parsed.success) {
-    throw new FetchError('UPDATE_PASSWORD_INVALID_DATA', 'Update password failed schema validation');
-  }
-
-  return parsed.data;
 }
