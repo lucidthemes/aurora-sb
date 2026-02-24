f3ae97eae8e63f53e7d32325796e7dcb29b40e3c
Lucid Themes <lucidtemplates@gmail.com>
2026-02-24 12:32:23 +0000

    refactor(newsletterForm): convert newsletter form hook to use tanstack query

--- src/features/newsletterForm/useNewsletterForm.ts
+++ src/features/newsletterForm/useNewsletterForm.ts
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
 
 import { NewsletterFormSchema } from '@schemas/newsletter/newsletter.schema';
 import type { NewsletterForm } from '@schemas/newsletter/newsletter.schema';
+import { createNewsletterSubscriber } from '@server/newsletter/createNewsletterSubscriber';
+import { FetchError } from '@services/errors/fetchError';
+import { createLogEvent } from '@services/logs/createLogEvent';
 import type { FormNotification } from '@typings/forms/notification';
 
 export default function useNewsletterForm() {
@@ -29,15 +33,32 @@ export default function useNewsletterForm() {
     resolver: zodResolver(NewsletterFormSchema),
   });
 
+  const newsletterFormMutation = useMutation({
+    mutationFn: createNewsletterSubscriber,
+    onSuccess: (_, variables) => {
+      setNewsletterFormNotification({
+        type: 'success',
+        message: 'Subscribed',
+      });
+
+      createLogEvent('info', 'CREATE_NEWSLETTER_SUBSCRIBER_SUCCESSFUL', 'Newsletter subscriber: ' + variables.email);
+
+      reset();
+    },
+    onError: (error: FetchError, variables) => {
+      if (error.message.includes('duplicate')) error.message = 'Email already subscribed';
+
+      setNewsletterFormNotification({
+        type: 'error',
+        message: error.message,
+      });
+
+      createLogEvent('error', error.code, error.message + '. Email: ' + variables.email);
+    },
+  });
+
   const onSubmit = async (data: NewsletterForm) => {
-    console.log(data); // temp
-
-    setNewsletterFormNotification({
-      type: 'success',
-      message: 'Subscribed',
-    });
-
-    reset();
+    newsletterFormMutation.mutate(data);
   };
 
   return {
