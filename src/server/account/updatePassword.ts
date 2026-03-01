4dfaba72b57e5e0ebd9e338de8ae3ef4a362aedc
Lucid Themes <lucidtemplates@gmail.com>
2026-03-01 12:59:55 +0000

    refactor(account): move password form hook create log event call to server

--- src/server/account/updatePassword.ts
+++ src/server/account/updatePassword.ts
@@ -1,13 +1,25 @@
 import { supabase } from '@lib/supabase/client';
+import type { User } from '@supabase/supabase-js';
 import type { DetailsPasswordForm } from '@schemas/account/detailsPassword.schema';
-import { FetchError } from '@services/errors/fetchError';
+import { createLogEvent } from '@services/logs/createLogEvent';
 
-export async function updateAccountDetailsPassword(formData: DetailsPasswordForm) {
+interface UpdateAccountDetailsPasswordParams {
+  user: User;
+  formData: DetailsPasswordForm;
+}
+
+export async function updateAccountDetailsPassword({ user, formData }: UpdateAccountDetailsPasswordParams) {
   const { error } = await supabase.auth.updateUser({
     password: formData.password,
   });
 
   if (error) {
-    throw new FetchError('UPDATE_PASSWORD_FAILED', error.message);
+    createLogEvent('error', 'UPDATE_PASSWORD_FAILED', error.message, user?.id);
+
+    return { success: false };
   }
+
+  createLogEvent('info', 'UPDATE_PASSWORD_SUCCESSFUL', 'Password updated', user?.id);
+
+  return { success: true };
 }
