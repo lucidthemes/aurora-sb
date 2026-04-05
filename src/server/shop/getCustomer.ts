09cd59f707dd4897b31e7775be09196d30042f97
Lucid Themes <lucidtemplates@gmail.com>
2026-04-05 15:46:07 +0100

    refactor(server): add create log event to shop get customer

--- src/server/shop/getCustomer.ts
+++ src/server/shop/getCustomer.ts
@@ -2,23 +2,29 @@ import { supabase } from '@lib/supabase/client';
 
 import { CustomerSchema } from '@schemas/shop/customer.schema';
 import type { Customer } from '@schemas/shop/customer.schema';
-import { FetchError } from '@services/errors/fetchError';
+import { createLogEvent } from '@lib/supabase/logEvent';
 
-export async function getCustomer(id: string): Promise<Customer | undefined> {
+export async function getCustomer(id: string): Promise<Customer | null> {
   const { data, error } = await supabase.from('customers').select('first_name, last_name, shipping_address, billing_address').eq('id', id).maybeSingle();
 
   if (error) {
-    throw new FetchError('FETCH_CUSTOMER_FAILED', error.message);
+    await createLogEvent('error', 'FETCH_CUSTOMER_FAILED', error.message);
+
+    return null;
   }
 
   if (!data) {
-    throw new FetchError('FETCH_CUSTOMER_NO_USER', 'No user found');
+    await createLogEvent('error', 'FETCH_CUSTOMER_NO_USER', 'No user found');
+
+    return null;
   }
 
   const parsed = CustomerSchema.safeParse(data);
 
   if (!parsed.success) {
-    throw new FetchError('FETCH_CUSTOMER_INVALID_DATA', 'Get customer failed schema validation');
+    await createLogEvent('error', 'FETCH_CUSTOMER_INVALID_DATA', 'Get customer failed schema validation');
+
+    return null;
   }
 
   return parsed.data;
