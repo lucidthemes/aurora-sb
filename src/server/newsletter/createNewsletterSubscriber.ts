5bcf3d4532989a2ab7894533881ea6aef9a1f43a
Lucid Themes <lucidtemplates@gmail.com>
2026-04-05 15:31:05 +0100

    refactor(newsletterForm): move create log event to server

--- src/server/newsletter/createNewsletterSubscriber.ts
+++ src/server/newsletter/createNewsletterSubscriber.ts
@@ -1,12 +1,18 @@
 import { supabase } from '@lib/supabase/client';
 
 import type { NewsletterForm } from '@schemas/newsletter/newsletter.schema';
-import { FetchError } from '@services/errors/fetchError';
+import { createLogEvent } from '@lib/supabase/logEvent';
 
 export async function createNewsletterSubscriber(formData: NewsletterForm) {
   const { error } = await supabase.from('newsletter_subscribers').insert({ email: formData.email });
 
   if (error) {
-    throw new FetchError('CREATE_NEWSLETTER_SUBSCRIBER_FAILED', error.message);
+    await createLogEvent('error', 'CREATE_NEWSLETTER_SUBSCRIBER_FAILED', error.message);
+
+    return { success: false };
   }
+
+  await createLogEvent('info', 'CREATE_NEWSLETTER_SUBSCRIBER_SUCCESSFUL', 'Newsletter subscriber: ' + formData.email);
+
+  return { success: true };
 }
