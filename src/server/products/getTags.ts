17183a6c5053bd5e8d429999520a2458a5ffff3f
Lucid Themes <lucidtemplates@gmail.com>
2026-05-18 13:15:09 +0100

    fix(server): get product tags using incorrect schema

--- src/server/products/getTags.ts
+++ src/server/products/getTags.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-import { TagSchema } from '@schemas/posts/tag.schema';
+import { TagSchema } from '@schemas/products/tag.schema';
 import type { Tag } from '@typings/products/tag';
 
 export async function getTagsArray(tagIds: number[]): Promise<Tag[]> {
