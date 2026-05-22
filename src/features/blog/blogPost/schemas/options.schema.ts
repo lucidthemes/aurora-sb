8d0c8be9847773c8a361d44ca3070f67f6f19bed
Lucid Themes <lucidtemplates@gmail.com>
2026-05-22 15:53:40 +0100

    refactor(blogPost): change post options structure

--- src/features/blog/blogPost/schemas/options.schema.ts
+++ src/features/blog/blogPost/schemas/options.schema.ts
@@ -2,10 +2,15 @@ import { z } from 'zod';
 
 export const PostOptionsSchema = z.object({
   header: z.object({
+    show: z.boolean(),
     layout: z.enum(['outside-above', 'outside-below', 'split-narrow', 'split-wide', 'split-full', 'overlay-narrow', 'overlay-wide', 'overlay-full']),
     besideSidebar: z.boolean(),
   }),
-  sidebar: z.enum(['left', 'right', 'hidden']),
+  sidebar: z.object({
+    show: z.boolean(),
+    option: z.string(),
+    position: z.enum(['left', 'right']),
+  }),
 });
 
 export type PostOptions = z.infer<typeof PostOptionsSchema>;
