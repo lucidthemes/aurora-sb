045bc94793c182d213731309ccb30a54f1597d2b
Lucid Themes <lucidtemplates@gmail.com>
2026-05-26 16:56:19 +0100

    refactor(footer): footer main to use sidebars fetched from supabase

--- src/components/Footer/FooterMain/FooterMain.tsx
+++ src/components/Footer/FooterMain/FooterMain.tsx
@@ -1,17 +1,14 @@
 import Container from '@components/Layout/Container';
-import { AboutWidget, PostsWidget, ProductsWidget } from '@components/Widgets';
+import Sidebar from '@features/sidebar';
 
 export default function FooterMain() {
   return (
     <div className="bg-pampas py-10">
       <Container>
         <div className="grid grid-cols-1 gap-x-7.5 gap-y-10 md:grid-cols-3">
-          <AboutWidget
-            title="About me"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque nibh enim, quis euismod enim lacinia nec. Phasellus quam diam, semper in erat eu, efficitur molestie purus. Sed a elementum mi. Sed interdum mattis risus, sit amet eleifend ligula luctus ut."
-          />
-          <PostsWidget title="Latest posts" limit={3} style="small" location="footer" />
-          <ProductsWidget title="Latest products" limit={2} style="small" />
+          <Sidebar name="footer-1" aside={false} />
+          <Sidebar name="footer-2" aside={false} />
+          <Sidebar name="footer-3" aside={false} />
         </div>
       </Container>
     </div>
