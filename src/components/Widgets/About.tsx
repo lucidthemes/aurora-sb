f07d17fafe9cc84c3bf97bacaf3b1d952b5e806f
Lucid Themes <lucidtemplates@gmail.com>
2026-09-09 15:22:42 +0100

    refactor(widgets): update widgets image url

--- src/components/Widgets/About.tsx
+++ src/components/Widgets/About.tsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 
 import WidgetTitle from '@components/Widgets/Title';
 import SocialIcons from '@components/UI/SocialIcons';
-import { getPublicMediaUrl } from '@lib/supabase/storage';
 
 interface AboutWidgetProps {
   title?: string;
@@ -27,22 +26,16 @@ export default function AboutWidget({
   centered = true,
   padding = true,
 }: AboutWidgetProps) {
-  let backgroundMediaUrl = '';
-  let authorMediaUrl = '';
-
-  if (backgroundImage) backgroundMediaUrl = getPublicMediaUrl(backgroundImage);
-  if (authorImage) authorMediaUrl = getPublicMediaUrl(authorImage);
-
   return (
     <section className={`about-widget ${centered ? 'text-center' : ''} ${padding ? 'rounded-md bg-pampas p-5' : ''} `}>
       <WidgetTitle>{title}</WidgetTitle>
-      {backgroundMediaUrl && <img src={backgroundMediaUrl} alt={heading} className="rounded-md" />}
-      {authorMediaUrl && (
+      {backgroundImage && <img src={backgroundImage} alt={heading} className="rounded-md" />}
+      {authorImage && (
         <Link
           to={link}
           className="relative mx-auto mt-[-75px] mb-5 block h-35 w-35 overflow-hidden rounded-full border-4 border-white bg-cover bg-center transition-colors duration-300 ease-in-out hover:border-pearl-bush focus:border-pearl-bush"
         >
-          <img src={authorMediaUrl} alt={heading} className="h-full w-full" />
+          <img src={authorImage} alt={heading} className="h-full w-full" />
         </Link>
       )}
       <div className="flex flex-col gap-y-5">
--- src/components/Widgets/PromoBox.tsx
+++ src/components/Widgets/PromoBox.tsx
@@ -1,6 +1,5 @@
 import WidgetTitle from '@components/Widgets/Title';
 import PromoBox from '@features/home/promoBox';
-import { getPublicMediaUrl } from '@lib/supabase/storage';
 
 interface PromoBoxWidgetProps {
   title?: string;
@@ -12,14 +11,10 @@ interface PromoBoxWidgetProps {
 }
 
 export default function PromoBoxWidget({ title = '', image, heading, subHeading, link, position = 'bottom' }: PromoBoxWidgetProps) {
-  let mediaUrl = '';
-
-  if (image) mediaUrl = getPublicMediaUrl(image);
-
   return (
     <section>
       <WidgetTitle>{title}</WidgetTitle>
-      <PromoBox image={mediaUrl} heading={heading} headingLevel="4" subHeading={subHeading} link={link} position={position} />
+      <PromoBox image={image} heading={heading} headingLevel="4" subHeading={subHeading} link={link} position={position} />
     </section>
   );
 }
